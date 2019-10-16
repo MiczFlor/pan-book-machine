4491e9bd005f41c6fc9ba2f15562a7de0cbe9671
Micz Flor <micz.flor@web.de>
2019-10-16 18:10:39 +0200

    master cli for new book

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # pan-book-machine
+
 A little hack to make formatted documents from markdown using PanDoc https://pandoc.org/
+
+To create a new book, **go into the `pan-book-machine` and type:**
+~~~
+./cli_createBook.sh
+~~~
+
+Here you can provide
+
+* the folder name for the new book
+* decide if you want sample content (possibly a good idea if you want to play with pan-doc-machine)
+
+**No edit the file inside the newly created folder:**
+~~~
+CONFIG/book.conf
+~~~
+
+Each option (like PDF or DOCX you can set to TRUE or FALSE)
+
+**Finally, edit the file:**
+~~~
+CONFIG/metadata-info.yaml
+~~~
+To make the output match your information (title and the like)
---
+++ cli_createBook.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# The absolute path to the folder which contains this script
+PATHDATA="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+clear
+
+# ? Folder name ?
+read -r -p "Type folder name for the book: " folderName
+
+# ? Copy sample content ?
+read -r -p "Do you want to create sample content [y/N]? " response
+case "$response" in
+    [yY][eE][sS]|[yY])
+        sampeContent="YES"
+        ;;
+    *)
+        sampeContent="NO"
+        ;;
+esac
+
+# Create book folder
+if [ ! -d "${folderName}" ]; then
+    # Directory doesn't exist.
+    mkdir "${folderName}"
+    # Copy required scripts / files
+    cp 00-PanBookMachine-boilerplate/cli_pandocEbook.sh "${folderName}"/
+    mkdir "${folderName}"/CONTENT
+    mkdir "${folderName}"/CONFIG
+    cp -R 00-PanBookMachine-boilerplate/misc/examples/CONFIG/* "${folderName}"/CONFIG/
+    # replace book name with given folder name
+    sed -i 's/%TargetBook%/'"${folderName}"'/' "${folderName}"/CONFIG/book.conf
+    mkdir "${folderName}"/misc
+    mkdir "${folderName}"/tmp
+else
+    echo
+    echo "Folder '${folderName}' already exists. Exiting."
+    exit
+fi
+
+# Copy sample content
+if [ ${sampeContent} == "YES" ]; then
+    echo "Copying sample content."
+    cp -R 00-PanBookMachine-boilerplate/CONTENT/* "${folderName}"/CONTENT/
+else
+    echo "Not copying sample content."
+fi
